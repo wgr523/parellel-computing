79273f7654129982c1a2b1e7874cfb596d41298b
wgr523 <wgr523@gmail.com>
2019-06-09 18:47:53 -0500

    test2

--- main.cpp
+++ main.cpp
@@ -28,7 +28,7 @@ void fft(CArray& x) {
 
     size_t k;
     if (N > pow(2,4)) {
-    #pragma omp parallel for num_threads(8) schedule(dynamic)
+    //#pragma omp parallel for num_threads(8) schedule(dynamic)
         for (k = 0; k < N / 2; ++k) {
             Complex t = std::polar(1.0, -2 * PI * k / N) * odd[k];
             x[k] = even[k] + t;
@@ -63,6 +63,37 @@ void test1() {
     }
 }
 
+void test2() {
+    int k, N;
+    N=100000;
+    Complex data[N];
+    for (long k = 0; k < N; ++k) {
+        data[k].real(1);
+        data[k].imag(1);
+    }
+    CArray arr(data, N);
+    CArray even;
+    CArray  odd;
+
+    auto start = Clock::now();
+
+    for(int i = 0; i <= 1; i++) {
+        CArray temp = arr[std::slice(i, N / 2, 2)];
+    }
+    auto end = Clock::now();
+    std::cout<<"T0: "<< std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() << " nanoseconds \n\n";
+    start = Clock::now();
+    start = Clock::now();
+
+#pragma omp parallel for
+    for(int i = 0; i <= 1; i++) {
+        CArray temp = arr[std::slice(i, N / 2, 2)];
+    }
+    end = Clock::now();
+    std::cout<<"T1: "<< std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() << " nanoseconds \n\n";
+    start = Clock::now();
+
+}
 int main()
 {
     long size = pow(2, 15);
@@ -77,8 +108,9 @@ int main()
     auto start = Clock::now();
 
     // forward fft
-    fft(arr);
-    //test1();
+    // fft(arr);
+    // test1();
+    test2();
 
     auto end = Clock::now();
 
